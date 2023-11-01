0ae0ba0218fcccaf658b45dc1748ef64ebfdb959
oosakiharuki <146906560+oosakiharuki@users.noreply.github.com>
2023-11-01 12:50:52 +0900

    変えた

--- main.cpp
+++ main.cpp
@@ -5,12 +5,7 @@ int main()
 	printf("LC1B_oosaki_haruki\n");
 	printf("2023/10/11\n");
 
-	printf("プルリクエストを停止します\n");
-
-	//player作成
-	printf("player\n");
-
-	printf("マージしてください");
+	printf("プルリクエストを開始しません\n");
 
 	return 0;
 
