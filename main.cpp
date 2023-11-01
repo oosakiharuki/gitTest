c6701297f9eac93a8ed9d7cec936a1c7a3fdb98c
oosakiharuki <k023g0035@g.neec.ac.jp>
2023-11-01 12:48:00 +0900

    プルリクエスト練習

--- main.cpp
+++ main.cpp
@@ -5,12 +5,7 @@ int main()
 	printf("LC1B_oosaki_haruki\n");
 	printf("2023/10/11\n");
 
-	printf("プルリクエストを開始する\n");
-
-	//player菴懈・
-	printf("player\n");
-
-	printf("繝槭・繧ｸ縺励※縺上□縺輔＞");
+	printf("プルリクエストを開始します\n");
 
 	return 0;
 
